ccf28babc8337f09cfcfe13505165465466ad356
duytruc98 <83696278+duytruc98@users.noreply.github.com>
2021-05-11 10:53:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello guy");
+    response.end("Hello baibe");
 });
 
 const port = process.env.PORT || 1337;
